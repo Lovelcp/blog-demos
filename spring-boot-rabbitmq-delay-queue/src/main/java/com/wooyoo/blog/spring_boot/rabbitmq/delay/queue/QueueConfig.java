8197f3b0df50c6a02eff6bf93f5e13b9e0474e96
kissrain <cjyang1123@gmail.com>
2017-10-11 22:16:10 +0800

    code refactor and add delay queue per queue

--- spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/QueueConfig.java
+++ spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/QueueConfig.java
@@ -15,32 +15,49 @@ import org.springframework.context.annotation.Configuration;
 public class QueueConfig {
     /**
      * 发送到该队列的job会在一段时间后过期进入到delay_process_queue
+     * 每个message可以控制自己的失效时间
      */
-    final static String delayQueueName = "delay_queue";
+    final static String DELAY_QUEUE_PER_MESSAGE_NAME = "delay_queue_per_message";
 
     /**
-     * 进入到该队列的任务会被消费
+     * 发送到该队列的job会在一段时间后过期进入到delay_process_queue
+     * 队列里所有的message都有统一的失效时间
      */
-    final static String delayProcessQueueName = "delay_process_queue";
+    final static String DELAY_QUEUE_PER_QUEUE_NAME = "delay_queue_per_queue";
+    final static int QUEUE_EXPIRATION = 4000;
 
-    final static String delayExchangeName = "delay_exchange";
+    /**
+     * message失效后进入到的队列
+     */
+    final static String DELAY_PROCESS_QUEUE_NAME = "delay_process_queue";
+
+    final static String DELAY_EXCHANGE_NAME = "delay_exchange";
 
     @Bean
     DirectExchange delayExchange() {
-        return new DirectExchange(delayExchangeName);
+        return new DirectExchange(DELAY_EXCHANGE_NAME);
     }
 
     @Bean
-    Queue delayQueue() {
-        return QueueBuilder.durable(delayQueueName)
-                           .withArgument("x-dead-letter-exchange", delayExchangeName) // dead letter发送到的exchange
-                           .withArgument("x-dead-letter-routing-key", delayProcessQueueName) // dead letter携带的routing key
+    Queue delayQueuePerMessage() {
+        return QueueBuilder.durable(DELAY_QUEUE_PER_MESSAGE_NAME)
+                           .withArgument("x-dead-letter-exchange", DELAY_EXCHANGE_NAME) // dead letter发送到的exchange
+                           .withArgument("x-dead-letter-routing-key", DELAY_PROCESS_QUEUE_NAME) // dead letter携带的routing key
+                           .build();
+    }
+
+    @Bean
+    Queue delayQueuePerQueue() {
+        return QueueBuilder.durable(DELAY_QUEUE_PER_QUEUE_NAME)
+                           .withArgument("x-dead-letter-exchange", DELAY_EXCHANGE_NAME) // dead letter发送到的exchange
+                           .withArgument("x-dead-letter-routing-key", DELAY_PROCESS_QUEUE_NAME) // dead letter携带的routing key
+                           .withArgument("x-message-ttl", QUEUE_EXPIRATION)
                            .build();
     }
 
     @Bean
     Queue delayProcessQueue() {
-        return QueueBuilder.durable(delayProcessQueueName)
+        return QueueBuilder.durable(DELAY_PROCESS_QUEUE_NAME)
                            .build();
     }
 
@@ -48,7 +65,7 @@ public class QueueConfig {
     Binding binding(Queue delayProcessQueue, DirectExchange delayExchange) {
         return BindingBuilder.bind(delayProcessQueue)
                              .to(delayExchange)
-                             .with(delayProcessQueueName);
+                             .with(DELAY_PROCESS_QUEUE_NAME);
     }
 
     /**
@@ -62,7 +79,7 @@ public class QueueConfig {
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(delayProcessQueueName); // 监听delay_process_queue
+        container.setQueueNames(DELAY_PROCESS_QUEUE_NAME); // 监听delay_process_queue
         container.setMessageListener(listenerAdapter);
         return container;
     }
--- spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/Runner.java
+++ spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/Runner.java
@@ -7,14 +7,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class Runner implements CommandLineRunner {
     private final RabbitTemplate rabbitTemplate;
-    private static final int EXPIRATION = 4000;
 
     public Runner(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void run(String... args) throws Exception {
-        System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(QueueConfig.delayQueueName, (Object) "Hello from RabbitMQ!", new ExpirationMessagePostProcessor(EXPIRATION));
+        testDelayQueuePerMessage();
+        testDelayQueuePerQueue();
+    }
+
+    public void testDelayQueuePerMessage() {
+        System.out.println("Sending message to delay_queue_per_message...");
+        for (int i = 1; i < 4; i++) {
+            int expiration = i * 1000;
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_PER_MESSAGE_NAME,
+                    (Object) ("Message From delay_queue_per_message with expiration " + expiration), new ExpirationMessagePostProcessor(expiration));
+        }
+    }
+
+    public void testDelayQueuePerQueue() {
+        System.out.println("Sending message to delay_queue_per_queue...");
+        for (int i = 1; i < 4; i++) {
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_PER_QUEUE_NAME,
+                    ("Message From delay_queue_per_queue with expiration " + QueueConfig.QUEUE_EXPIRATION));
+        }
     }
 }
