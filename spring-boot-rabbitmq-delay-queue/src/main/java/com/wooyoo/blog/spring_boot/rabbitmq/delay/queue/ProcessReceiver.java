e6b4fbb3539e64631f69dd61d324e6d0132773c2
kissrain <cjyang1123@gmail.com>
2017-10-16 22:00:48 +0800

    refactor to junit test case

--- spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/ProcessReceiver.java
+++ spring-boot-rabbitmq-delay-queue/src/main/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/ProcessReceiver.java
@@ -2,9 +2,16 @@ package com.wooyoo.blog.spring_boot.rabbitmq.delay.queue;
 
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CountDownLatch;
+
 @Component
 public class ProcessReceiver {
+    public static CountDownLatch latch;
+
     public void processMessage(String message) {
         System.out.println("Received <" + message + ">");
+        if (latch != null) {
+            latch.countDown();
+        }
     }
 }
--- spring-boot-rabbitmq-delay-queue/src/test/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/SpringBootRabbitmqDelayQueueApplicationTests.java
+++ spring-boot-rabbitmq-delay-queue/src/test/java/com/wooyoo/blog/spring_boot/rabbitmq/delay/queue/SpringBootRabbitmqDelayQueueApplicationTests.java
@@ -2,15 +2,56 @@ package com.wooyoo.blog.spring_boot.rabbitmq.delay.queue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootRabbitmqDelayQueueApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
+    @Test
+    public void testDelayQueuePerMessage() throws InterruptedException {
+        ProcessReceiver.latch = new CountDownLatch(3);
+        for (int i = 1; i <= 3; i++) {
+            int expiration = i * 1000;
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_PER_MESSAGE_NAME,
+                    (Object) ("Message From delay_queue_per_message with expiration " + expiration), new ExpirationMessagePostProcessor(expiration));
+        }
+        ProcessReceiver.latch.await();
+    }
+
+    @Test
+    public void testDelayQueuePerQueue() throws InterruptedException {
+        ProcessReceiver.latch = new CountDownLatch(3);
+        for (int i = 1; i <= 3; i++) {
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_PER_QUEUE_NAME,
+                    ("Message From delay_queue_per_queue with expiration " + QueueConfig.QUEUE_EXPIRATION));
+        }
+        ProcessReceiver.latch.await();
+    }
+
+    @Test
+    public void testDelayQueueReject() throws InterruptedException {
+        ProcessReceiver.latch = new CountDownLatch(3);
+        for (int i = 1; i <= 3; i++) {
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_REJECT_NAME, "Message From delay_queue_reject");
+        }
+        ProcessReceiver.latch.await();
+    }
+
+    @Test
+    public void testDelayQueueMaxLength() throws InterruptedException {
+        ProcessReceiver.latch = new CountDownLatch(3);
+        for (int i = 1; i <= 6; i++) {
+            rabbitTemplate.convertAndSend(QueueConfig.DELAY_QUEUE_MAX_LENGTH_NAME, "Message From delay_queue_max_length");
+        }
+        ProcessReceiver.latch.await();
+    }
 }
