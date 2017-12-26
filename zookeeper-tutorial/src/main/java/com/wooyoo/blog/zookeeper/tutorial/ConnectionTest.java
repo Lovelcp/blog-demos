2ee8ba33b7f08af44cda8a966d9f92635813595c
kissrain <cjyang1123@gmail.com>
2017-12-26 23:00:52 +0800

    add exists delete create and read test

--- zookeeper-tutorial/src/main/java/com/wooyoo/blog/zookeeper/tutorial/ConnectionTest.java
+++ zookeeper-tutorial/src/main/java/com/wooyoo/blog/zookeeper/tutorial/ConnectionTest.java
@@ -13,13 +13,24 @@ public class ConnectionTest {
         user.setId(1);
         user.setName("testUser");
 
+        String nodePath = "/testUserNode";
+
+        // 判断节点是否存在，存在则删除，不存在则创建
+        if (zkClient.exists(nodePath)) {
+            zkClient.delete(nodePath);
+        }
+
         /*
          * "/testUserNode" :节点的地址
          * user：数据的对象
          * CreateMode.PERSISTENT：创建的节点类型
          */
-        String path = zkClient.create("/testUserNode", user, CreateMode.PERSISTENT);
+        String path = zkClient.create(nodePath, user, CreateMode.PERSISTENT);
         //输出创建节点的路径
         System.out.println("created path:" + path);
+
+        // 获取刚才创建的User对象
+        User userResult = zkClient.readData(nodePath);
+        System.out.println(userResult.getName());
     }
 }
